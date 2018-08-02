7bc05acc540ef9c7d3801e468cf0d9133ae3b59d
yasuhiro <yasuhiro@DESKTOP-NUD0PO3>
2018-08-02 11:03:54 +0900

    2.2.2 データソースの設定を明示的に変更

--- hajiboot-jdbc/src/main/resources/data.sql
+++ hajiboot-jdbc/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO customers(first_name, last_name) VALUES("Juzo", "Okita");
-INSERT INTO customers(first_name, last_name) VALUES("Shiro", "Sanada");
-INSERT INTO customers(first_name, last_name) VALUES("Susumu", "Kodai");
-INSERT INTO customers(first_name, last_name) VALUES("Daisuke", "Shima");
+INSERT INTO customers(first_name, last_name) VALUES('Juzo', 'Okita');
+INSERT INTO customers(first_name, last_name) VALUES('Shiro', 'Sanada');
+INSERT INTO customers(first_name, last_name) VALUES('Susumu', 'Kodai');
+INSERT INTO customers(first_name, last_name) VALUES('Daisuke', 'Shima');
