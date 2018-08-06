ec73ce7ed1e313f26b9950c318b1fc8ddbbd7f26
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-06 17:58:05 +0900

    2.3.4 ページング処理の実装

--- hajiboot-jpa/src/main/java/com/example/repository/CustomerRepository.java
+++ hajiboot-jpa/src/main/java/com/example/repository/CustomerRepository.java
@@ -6,8 +6,10 @@ package com.example.repository;
 
 import com.example.domain.Customer;
 
-import java.util.List;
+//import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,5 +18,6 @@ public interface CustomerRepository extends JpaRepository <Customer, Integer> {
 	// JPQLを記述
 	@Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
 	
-	List<Customer> findAllOrderByName();
+//	List<Customer> findAllOrderByName();
+	Page<Customer> findAllOrderByName(Pageable pageable);
 }
