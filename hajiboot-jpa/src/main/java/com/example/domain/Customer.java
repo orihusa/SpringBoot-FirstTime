79390be053732f78a745de3e1c3197a57a7fb898
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-07 23:17:17 +0900

    2.3.4 ページング処理の実装（実行確認） GeneratedValueが無効になる件の対策

--- hajiboot-jpa/src/main/java/com/example/domain/Customer.java
+++ hajiboot-jpa/src/main/java/com/example/domain/Customer.java
@@ -21,7 +21,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor	// 全フィールドを引数にもつコンストラクタを生成させる
 public class Customer {
 	@Id						// Entityの主キーに付ける
-	@GeneratedValue(strategy=GenerationType.IDENTITY)			// DBで主キーが自動採番されることを示す
+	@GeneratedValue(strategy=GenerationType.IDENTITY)		// DBで主キーが自動採番されることを示す
 	private Integer id;
 	
 	@Column(name = "first_name", nullable = false)
