7939683eac31e307270b462834cac2274c9ef200
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-08 17:53:17 +0900

    3.2.2 顧客新規作成、顧客１件更新、顧客１件削除APIの実装

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -4,9 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.Customer;
@@ -18,17 +24,38 @@ public class CustomerRestController {
 	@Autowired
 	CustomerService customerService;
 
-	// a顧客全件取得
+	// a.顧客全件取得
 	@GetMapping
 	List<Customer> getCustomers() {
 		List<Customer> customers = customerService.findAll();
 		return customers;
 	}
 	
-	// b顧客１件取得
+	// b.顧客１件取得
 	@GetMapping(path = "{id}")
 	Optional<Customer> getCustomer(@PathVariable Integer id) {
 		Optional<Customer> customer = customerService.findOne(id);
 		return customer;
+	}
+
+	// c.新規作成
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	Customer postCustomers(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+
+	// d.顧客1件更新
+	@PutMapping(path = "{id}")
+	Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+		customer.setId(id);
+		return customerService.update(customer);
+	}
+
+	// e.顧客１件削除
+	@DeleteMapping(path = "{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void deleteCustomer(@PathVariable Integer id) {
+		customerService.delete(id);
 	}
 }
