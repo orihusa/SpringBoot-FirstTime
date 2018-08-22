da5527ada5fb5b0cae6d6081be77ef4f13db6705
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-22 14:01:50 +0900

    3.3.3 顧客情報の編集

--- hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
+++ hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
@@ -49,4 +50,36 @@ public class CustomerController {
 		customerService.create(customer);
 		return "redirect:/customers";	// 正常終了時、一覧画面表示にリダイレクトする 
 	}
+
+	@GetMapping(path="edit", params="form")
+	String editForm(@RequestParam	// 特定のリクエストパラメータをマッピング
+					Integer id,		// リクエストパラメータのidをマッピング
+					CustomerForm form) {
+		Customer customer = customerService.findOne(id);
+		BeanUtils.copyProperties(customer, form);
+		return "customers/edit";		// 顧客情報編集画面
+	}
+	
+	@PostMapping(path="edit")
+	String edit(@RequestParam
+					Integer id,
+					@Validated CustomerForm form,
+					BindingResult result) {
+		// 送信されたCustomerFormをCustomerにコピーする
+		Customer customer = new Customer();
+		BeanUtils.copyProperties(form, customer);
+		// 更新処理を実施
+		customer.setId(id);
+		customerService.update(customer);
+		return "redirect:/customers";	// 処理が完了したら一覧表示画面にリダイレクトする
+	}
+	
+	@PostMapping(path="edit", params="goToTop")
+	String goToTop() {
+		return "redirect:/customers";
+	}
+
+
+
+
 }
