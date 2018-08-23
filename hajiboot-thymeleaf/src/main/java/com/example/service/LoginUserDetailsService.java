0046edd17c2b23ac608d8763b7c2613e7e4b7ae6
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-23 11:04:24 +0900

    3.5.2 認証ユーザの作成

---
+++ hajiboot-thymeleaf/src/main/java/com/example/service/LoginUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.User;
+import com.example.repository.UserRepository;
+
+@Service	// コンポーネントスキャンの対象とする
+public class LoginUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findOne(username);	// ユーザ名からUserオブジェクト取得
+		if (user ==  null) {
+			// Userオブジェクトが見つからない場合、例外UsernameNotFoundExceptionを発生させる
+			throw new UsernameNotFoundException("The requested user is not found.");
+		}
+		return new LoginUserDetails(user);
+	}
+}
