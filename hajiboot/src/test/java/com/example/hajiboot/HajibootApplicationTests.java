962d1222d61d04a52c1aaa9a8b3f412b8715efa6
orihusa <blue_corundum@osu.bbiq.jp>
2018-08-27 13:17:24 +0900

    【５．１】Hello Worldアプリの結合テスト

--- hajiboot/src/test/java/com/example/hajiboot/HajibootApplicationTests.java
+++ hajiboot/src/test/java/com/example/hajiboot/HajibootApplicationTests.java
@@ -1,16 +1,35 @@
 package com.example.hajiboot;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//Spring Bootテストの有効化。RANDOM_PORT：空いているポートに組み込みサーバを立ち上げる。
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HajibootApplicationTests {
-
+	@LocalServerPort
+	int port;	// 立ち上がったサーバのポート番号を注入
+	
+	@Autowired
+	TestRestTemplate restTemplate;
+	
 	@Test
 	public void contextLoads() {
-	}
+		ResponseEntity<String> response = restTemplate.getForEntity(
+				"http://localhost:" + port, String.class);
+		// TestRestTemplateを使用する場合、「プロトコル://ホスト名:ポート番号」を省略する事ができる
+		//ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("Hello World TEST!");
+	}
 }
